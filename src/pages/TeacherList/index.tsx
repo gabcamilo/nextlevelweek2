f8039b4aca7cb1d3dd85198b156d24b1d3a5d137
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2020-08-10 20:52:18 -0300

    TeacherList component.

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logoImg from '../../assets/images/logo.svg'
+import backIcon from '../../assets/images/icons/back.svg'
+
+import './styles.css'
 
 function TeacherList () {
 	return(
-		<div id="page-teacher-list">
-			TeacherList
+		<div id="page-teacher-list" className="container">
+			<header className="page-header">
+				<div className="top-bar-container">
+					<Link to="/">
+						<img src={backIcon} alt="go back to landing page"/>
+					</Link>
+					<img src={logoImg} alt="logo proffy"/>
+				</div>
+
+				<div className="header-content">
+					<strong>Those are the available teachers.</strong>
+				</div>
+			</header>
 		</div>
 	);
 }
