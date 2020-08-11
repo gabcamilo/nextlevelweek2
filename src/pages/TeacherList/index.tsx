d1339acae72e0e9aee4a2ce01c36e2ddf5ef2ed9
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2020-08-10 21:48:27 -0300

    TeacherList's header (smaller screens layout).

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-import logoImg from '../../assets/images/logo.svg'
-import backIcon from '../../assets/images/icons/back.svg'
+import PageHeader from '../../components/PageHeader'
 
 import './styles.css'
 
 function TeacherList () {
 	return(
 		<div id="page-teacher-list" className="container">
-			<header className="page-header">
-				<div className="top-bar-container">
-					<Link to="/">
-						<img src={backIcon} alt="go back to landing page"/>
-					</Link>
-					<img src={logoImg} alt="logo proffy"/>
-				</div>
+			<PageHeader title="Those are the available teachers.">
+				<form id="search-teachers">
+					<div className="input-block">
+						<label htmlFor="subject">Subject</label>
+						<input type="text" id="subject"/>
+					</div>
+					
+					<div className="input-block">
+						<label htmlFor="week-day">Week Day</label>
+						<input type="text" id="week-day"/>
+					</div>
+					
+					<div className="input-block">
+						<label htmlFor="time">Time</label>
+						<input type="text" id="time"/>
+					</div>
 
-				<div className="header-content">
-					<strong>Those are the available teachers.</strong>
-				</div>
-			</header>
+				</form>
+			</PageHeader>
 		</div>
 	);
 }
