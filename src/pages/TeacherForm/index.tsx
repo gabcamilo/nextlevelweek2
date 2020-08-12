371dbf5bbf7264ba11c8e131bf8e9334ee2060cb
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2020-08-11 22:16:38 -0300

    TeacherForm component basics.

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import PageHeader from '../../components/PageHeader'
+
+// import './styles.css'
+
 function TeacherForm () {
 	return(
-		<div id="page-teacher-form">
-			TeacherForm
-		</div>
+		<div id="page-teacher-form" className="container">
+		<PageHeader title="It's amazing you wanna give classes!"/>
+	</div>
 	);
 }
 
