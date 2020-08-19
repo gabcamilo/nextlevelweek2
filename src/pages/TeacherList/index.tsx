38f09e59f662223061da7ad36bf3cec8557d1c72
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2020-08-19 09:33:16 -0300

    :construction: Added Select to TeacherList page.

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import PageHeader from '../../components/PageHeader'
 import TeacherItem from '../../components/TeacherItem';
 import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 import './styles.css';
 
@@ -11,8 +12,31 @@ function TeacherList () {
 		<div id="page-teacher-list" className="container">
 			<PageHeader title="Those are the available teachers.">
 				<form id="search-teachers">
-					<Input name= "subject" label="Subject"/>
-					<Input name= "week-day" label="Week Day"/>
+					<Select
+						name="subject"
+						label="Subject"
+						options={[
+							{value: 'Arts', label: 'Arts'},
+							{value: 'Philosophy', label: 'Philosophy'},
+							{value: 'Math', label: 'Math'},
+							{value: 'English', label: 'English'},
+							{value: 'Chemistry', label: 'Chemistry'},
+						]}
+					/>
+					<Select
+						name="week-day"
+						label="Week Day"
+						options={[
+							{value: '1', label: 'Monday'},
+							{value: '2', label: 'Tuesday'},
+							{value: '3', label: 'Wednesday'},
+							{value: '4', label: 'Thursday'},
+							{value: '5', label: 'Friday'},
+							{value: '6', label: 'Saturday'},
+							{value: '0', label: 'Sunday'},
+						]}
+					/>
+					
 					<Input name= "time" label="Time" type="time"/>
 				</form>
 			</PageHeader>
